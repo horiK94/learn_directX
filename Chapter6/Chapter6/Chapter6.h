27bd3d7878370f3486cfc0db64ecb85c7552d93e
horiK <hazemi94basic@gmail.com>
2020-11-21 20:58:11 +0900

    Chapter5の内容をコピーしたChapter6を作成

---
+++ Chapter6/Chapter6/Chapter6.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <Windows.h>
+#include <mmsystem.h>
+#include <d3dx9.h>
+#include <tchar.h>
+#include <dinput.h>
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+struct Model
+{
+	//メッシュ
+	LPD3DXMESH pMesh;
+	//マテリアルの配列
+	D3DMATERIAL9* pMaterial;
+	//テクスチャの配列
+	LPDIRECT3DTEXTURE9* pTexture;
+	//マテリアルの数
+	DWORD numMaterials;
+
+	//データが入っているかのフラグ
+	bool used;
+};
+
+//グローバル変数
+//他のソースコードでも使用できるようにexternをつける
+extern LPDIRECT3D9 g_pD3D;
+extern LPDIRECT3DDEVICE9 g_pd3DDeivece;
+
+extern float g_aspect;
+
+extern Model g_models[];
+
+//関数プロトタイプ宣言
+HRESULT InitD3DWindow(LPCTSTR winTitle, int w, int h);
+int LoadModel(LPCTSTR fileName);
+void RenderModel(int idx);
+const char* GetKeyState();
